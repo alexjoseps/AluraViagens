6a281bfb55c4be75ed14272ca33a7fc7b0bb7e2d
Alex Souza <alexjoseps@icloud.com>
2025-12-03 20:26:59 -0300

    chore: adiciona table view de viagens na view controller como extension

--- AluraViagens/ViewController.swift
+++ AluraViagens/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viagensTableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        viagensTableView.dataSource = self
     }
 
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
+        -> Int
+    {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+
+        var content = cell.defaultContentConfiguration()
+        content.text = "Viagem \(indexPath.row)"
+
+        cell.contentConfiguration = content
+
+        return cell
+    }
 
 }
-
