c8fb078b42acad266c533164cd967e30ace856ea
Alex Souza <alexjoseps@icloud.com>
2025-12-03 21:18:51 -0300

    chore: cria e adiciona cabecalho da table view

--- AluraViagens/ViewController.swift
+++ AluraViagens/ViewController.swift
@@ -16,9 +16,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
 
         viagensTableView.dataSource = self
+        viagensTableView.delegate = self
     }
 
 }
+
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
         -> Int
@@ -39,4 +41,16 @@ extension ViewController: UITableViewDataSource {
         return cell
     }
 
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = Bundle.main.loadNibNamed("HomeTableViewHeader", owner: self)?.first as? HomeTableViewHeader
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 300
+    }
 }
